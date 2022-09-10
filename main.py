f887467dcf7f29246bba32ce53ad6357471e00c9
alexius <zte.lans@gmail.com>
2022-09-10 03:42:09 +0300

    the main menu of the file manager

---
+++ main.py
@@ -0,0 +1,42 @@
+print('0 - Путь текущего каталога')
+print('1 - Создать файл')
+print('2 - Создать папку')
+print('3 - Удалить папку')
+print('4 - Удалить файл')
+print('5 - Показать содержимое каталога')
+print('6 - Показать только папки')
+print('7 - Показать только файлы')
+print('8 - Подняться на уровень выше')
+print('9 - Опусться на уровень ниже')
+print('"exit" - Выход \n')
+
+while True:
+    menu_numbers = input('Выберите нужный пункт меню: ')
+    if menu_numbers == 'exit':
+        break
+    elif menu_numbers == 0:
+        pass
+    elif menu_numbers == 1:
+        pass
+    elif menu_numbers == 2:
+        pass
+    elif menu_numbers == 3:
+        pass
+    elif menu_numbers == 4:
+        pass
+    elif menu_numbers == 5:
+        pass
+    elif menu_numbers == 6:
+        pass
+    elif menu_numbers == 7:
+        pass
+    elif menu_numbers == 8:
+        pass
+    elif menu_numbers == 9:
+        pass
+    else:
+        print("\033[31m\033[1m\033[3m {}\033[0m".format('Не верный пункт меню'))
+
+
+
+
